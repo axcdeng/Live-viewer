642a1e7dc07e6b847af9df19fc3e3fdef9714400
c#sp <surxsc@gmail.com>
2025-11-25 17:16:48 -0800

    fix: Correct closing tag for nav element

--- src/components/WordPressHeader.jsx
+++ src/components/WordPressHeader.jsx
@@ -161,7 +161,7 @@ export default function WordPressHeader() {
                         </a>
                     );
                 })}
-            </div>
+            </nav>
         </nav>
     );
 }
